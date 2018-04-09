827ec47d58ee98da56bfd22b90a9d45a40e10d2b
Wilberto Palomar <wilberto.palomar@gmail.com>
2018-04-09 11:04:37 +1000

    Removed DB Credentials

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
 export DB_PRD_HOST=devops-postgresql.csir29swajmw.ap-southeast-2.rds.amazonaws.com
-export DB_PRD_USER=devops
-export DB_PRD_PASS=Wilb181ab
+export DB_PRD_USER=
+export DB_PRD_PASS=
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
